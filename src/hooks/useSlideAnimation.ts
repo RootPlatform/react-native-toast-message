e73f3fea4e5506931850059e1b411a550edacdd0
Ben Kass <ben@benkass.com>
2024-03-27 10:40:28 -0700

    Remove resolve and reject from animate to avoid unhandled rejection

--- src/hooks/useSlideAnimation.ts
+++ src/hooks/useSlideAnimation.ts
@@ -4,6 +4,7 @@ import { Animated, Platform } from 'react-native';
 import { SpringAnimationProps, ToastPosition } from '../types';
 import { additiveInverseArray } from '../utils/array';
 import { useKeyboard } from './useKeyboard';
+import { useLogger } from '../contexts';
 
 type UseSlideAnimationParams = {
   position: ToastPosition;
@@ -47,26 +48,29 @@ export function useSlideAnimation({
 }: UseSlideAnimationParams) {
   const animatedValue = React.useRef(new Animated.Value(0));
   const { keyboardHeight } = useKeyboard();
+  const { log } = useLogger();
 
-  const animate = React.useCallback(({ toValue, onHidden}: { toValue: number; onHidden: () => void }) => 
-  new Promise((resolve, reject) => {
-    Animated.spring(animatedValue.current, {
-      ...animationProps,
-      toValue,
-      useNativeDriver,
-      friction: animationProps?.friction ? animationProps.friction : 8
-    }).start(({ finished }) => {
-      if (finished) {
-        resolve(true); // Resolve the promise when the animation finishes
-        if (toValue === 0) {
-          // Animated to value 0 - run onHidden
-          onHidden();
-        }
-      } else {
-        reject(new Error('Animation was interrupted')); // Reject if the animation was interrupted
-      }
-    });
-  }), [animationProps]);
+  const animate = React.useCallback(
+    ({ toValue, onHidden }: { toValue: number; onHidden: () => void }) =>
+      new Promise(() => {
+        Animated.spring(animatedValue.current, {
+          ...animationProps,
+          toValue,
+          useNativeDriver,
+          friction: animationProps?.friction ? animationProps.friction : 8,
+        }).start(({ finished }) => {
+          if (finished) {
+            if (toValue === 0) {
+              // Animated to value 0 - run onHidden
+              onHidden();
+            }
+          } else {
+            log('Animation was interrupted');
+          }
+        });
+      }),
+    [animationProps]
+  );
 
   const translateY = React.useMemo(() => animatedValue.current.interpolate({
     inputRange: [0, 1],
